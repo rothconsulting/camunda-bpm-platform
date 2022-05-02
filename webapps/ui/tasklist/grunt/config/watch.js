a5e42f42683b3231af23cfded076cd5cc4150cab
Tassilo Weidner <3015690+tasso94@users.noreply.github.com>
2022-05-02 17:09:38 +0200

    chore(webapps): move english translations into main repository
    related to CAM-14623

--- webapps/ui/tasklist/grunt/config/watch.js
+++ webapps/ui/tasklist/grunt/config/watch.js
@@ -42,7 +42,7 @@ module.exports = function(config, watchConf) {
 
   watchConf.tasklist_dependencies = {
     options: options,
-    files: ['<%= pkg.gruntConfig.enTranslationDir %>/**/*.json'],
+    files: ['ui/tasklist/client/en.json'],
     tasks: ['localescompile:tasklist_locales']
   };
 
